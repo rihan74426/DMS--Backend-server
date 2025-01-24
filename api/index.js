4c84e91a0881fd3cf1c3234ad4cc7ffad6d29760
Md Rihan NJ <rihannjna@gmail.com>
2025-01-24 18:18:59 +0600

    almost baby

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connectDB = require("./config/db");
+const connectDB = require("../config/db");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const companyRoutes = require("../routes/companyRoute");
